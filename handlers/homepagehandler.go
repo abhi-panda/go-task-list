8b912b762fe1fb7f53f2c3ff3670bf953b8d55ab
abhi-panda <abhishekfeeling08@gmail.com>
2020-02-11 13:58:12 +0530

    11th Feb 2020 13:57 IST homepage edited

--- handlers/homepagehandler.go
+++ handlers/homepagehandler.go
@@ -12,5 +12,5 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	log.Info("Reached the Home Page!")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Welcome to TaskList, use {/tasklist} for create,update,delete and {/tasklist/<type (all,alltodo,overdue,today)>}"))
+	w.Write([]byte("Welcome to TaskList, use {/tasklist} for create,update,delete and {/tasklist/<type (all,alltodo,overdue,today,|TaskTitle|)>}"))
 }
